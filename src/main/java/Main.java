c7d3f80edcd2937406b2d0aef9faeb2b06bce1e2
Zoonmy <pjm2571@naver.com>
2024-07-10 15:00:18 +0900

    feat: geckodriver 경로 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "./geckodriver");
+
         Crawler crawler = new Crawler();
 
         List<News> newsList = crawler.crawlingNews();
